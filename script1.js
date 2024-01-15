0334b4bac8e5ebe0d97a00d1d056abc937363ae0
ASIF-SADIQ <143731036+ASIF-SADIQ@users.noreply.github.com>
2024-01-16 00:50:02 +0500

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,51 @@
+gsap.to("#bg",{
+    scrollTrigger:{
+        scrub:1,
+    },
+    scale:1.5,
+}) ;
+gsap.to("#man",{
+    scrollTrigger:{
+        scrub:1,
+    },
+    scale:0.5,
+}) ;
+gsap.to("#mountain_left",{
+    scrollTrigger:{
+        scrub:1,
+    },
+    x:-500,
+}) ;
+gsap.to("#mountain_right",{
+    scrollTrigger:{
+        scrub:1,
+    },
+    x:+500,
+}) ;
+gsap.to("#clouds1",{
+    scrollTrigger:{
+        scrub:1,
+    },
+    x:+200,
+}) ;
+gsap.to("#clouds2",{
+    scrollTrigger:{
+        scrub:1,
+    },
+    x:-200,
+}) ;
+
+gsap.to("#text",{
+    
+    color:"#bfff00",
+    
+  
+    y:200,
+    scrollTrigger:{
+        scrub:1,
+    },
+
+  
+ 
+}) ;
+
